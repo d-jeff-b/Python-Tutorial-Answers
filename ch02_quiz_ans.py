6de0cc98f49394fd4cf020412fb03a8a18905e0c
majeffma <hyma@navifus.com>
2026-01-15 23:34:31 +0800

    ch02_quiz_answer

---
+++ ch02_quiz_ans.py
@@ -0,0 +1,8 @@
+#1. 建立一個變數 `attack` 為 50。
+attack = 50
+#2. 建立一個變數 `buff` 為 1.5。
+buff = 1.5
+#3. 計算總攻擊力（兩者相乘）。
+total_attack = attack * buff
+#4. 使用轉化術將結果轉為「說書人」格式，印出：「最終傷害值為：75」。
+print('最終傷害值為：'+ str(int(total_attack)))
